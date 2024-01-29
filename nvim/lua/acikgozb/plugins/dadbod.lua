5d39f85c6392afc553bd956d9a4a5df10d754f69
acikgozb <berkacikgoz@armut.com>
2024-01-29 11:14:31 +0300

    Remove omnifunc for dadbod
    Also, FileType auto command is updated to use db_autocomplete_group as cmdgroup.

--- nvim/lua/acikgozb/plugins/dadbod.lua
+++ nvim/lua/acikgozb/plugins/dadbod.lua
@@ -11,12 +11,14 @@ return {
 
 		vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("plenary.path").path.sep .. "db_ui"
 
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = {
-				"sql",
-			},
-			command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
-		})
+		-- vim.api.nvim_create_autocmd("FileType", {
+		-- 	pattern = {
+		-- 		"sql",
+		-- 	},
+		-- 	command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+		-- })
+
+		local db_autocomplete_group = vim.api.nvim_create_augroup("db_completion", { clear = true })
 
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = {
@@ -26,6 +28,7 @@ return {
 			callback = function()
 				vim.schedule(db_completion)
 			end,
+			group = db_autocomplete_group,
 		})
 
 		vim.keymap.set("n", "<Leader>db", vim.cmd.DBUIToggle)
