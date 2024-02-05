08ae137df3f48f3b8a98e1862517e0bfb1b9426c
acikgozb <berkacikgoz@armut.com>
2024-02-05 09:48:15 +0300

    Change Neovim theme to rose-pine
    The theme is changed back to rose-pine from catppuccin.
    In order to keep the previous themes as an option, they are commented out instead of removing completely.

--- nvim/lua/acikgozb/plugins/theme.lua
+++ nvim/lua/acikgozb/plugins/theme.lua
@@ -1,42 +1,85 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	-- "catppuccin/nvim",
+	-- name = "catppuccin",
+	-- config = function()
+	-- 	local catppuccin = require("catppuccin")
+	--
+	-- 	catppuccin.setup({
+	-- 		flavour = "mocha",
+	-- 		no_italic = false,
+	-- 		no_bold = false,
+	-- 		no_underline = false,
+	-- 		styles = {
+	-- 			comments = { "italic" },
+	-- 			conditionals = { "italic" },
+	-- 			loops = {},
+	-- 			functions = {},
+	-- 			keywords = {},
+	-- 			strings = {},
+	-- 			variables = {},
+	-- 			numbers = {},
+	-- 			booleans = {},
+	-- 			properties = {},
+	-- 			types = {},
+	-- 			operators = {},
+	-- 		},
+	-- 		integations = {
+	-- 			cmp = true,
+	-- 			gitsigns = true,
+	-- 			treesitter = true,
+	-- 		},
+	-- 	})
+	--
+	-- vim.cmd.colorscheme("catppuccin")
+	-- end,
+	"rose-pine/neovim",
+	name = "rose-pine",
 	config = function()
-		local catppuccin = require("catppuccin")
-
-		catppuccin.setup({
-			flavour = "mocha",
-			no_italic = false,
-			no_bold = false,
-			no_underline = false,
-			styles = {
-				comments = { "italic" },
-				conditionals = { "italic" },
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-				operators = {},
+		require("rose-pine").setup({
+			variant = "main",
+			dark_variant = "main",
+			dim_inactive_windows = false,
+			extend_background_behind_borders = true,
+			enable = {
+				terminal = true,
+				legacy_highlights = true,
+				migrations = true,
 			},
-			integations = {
-				cmp = true,
-				gitsigns = true,
-				treesitter = true,
+			styles = {
+				bold = true,
+				italic = true,
+				transparency = true,
+			},
+			groups = {
+				border = "muted",
+				link = "iris",
+				panel = "surface",
+				error = "love",
+				hint = "iris",
+				info = "foam",
+				note = "pine",
+				todo = "rose",
+				warn = "gold",
+				git_add = "foam",
+				git_change = "rose",
+				git_delete = "love",
+				git_dirty = "rose",
+				git_ignore = "muted",
+				git_merge = "iris",
+				git_rename = "pine",
+				git_stage = "iris",
+				git_text = "rose",
+				git_untracked = "subtle",
+				h1 = "iris",
+				h2 = "foam",
+				h3 = "rose",
+				h4 = "gold",
+				h5 = "pine",
+				h6 = "foam",
 			},
 		})
-
-		vim.cmd.colorscheme("catppuccin")
+		vim.cmd("colorscheme rose-pine")
 	end,
-	-- "rose-pine/neovim",
-	-- name = "rose-pine",
-	-- config = function()
-	-- 	vim.cmd("colorscheme rose-pine")
-	-- end,
 	-- "loctvl842/monokai-pro.nvim",
 	-- config = function()
 	-- 	require("monokai-pro").setup({
@@ -78,6 +121,34 @@ return {
 	-- 		override = function(c) end,
 	-- 	})
 	-- 	vim.cmd([[colorscheme monokai-pro]])
+	-- end,
+	-- "projekt0n/caret.nvim",
+	-- config = function()
+	-- 	require("caret").setup({
+	-- 		options = {
+	-- 			transparent = false,
+	-- 			inverted_signs = false,
+	-- 			styles = {
+	-- 				bold = true,
+	-- 				italic = true,
+	-- 				strikethrough = true,
+	-- 				undercurl = true,
+	-- 				underline = true,
+	-- 			},
+	-- 			inverse = {
+	-- 				match_paren = false,
+	-- 				visual = false,
+	-- 				search = false,
+	-- 			},
+	-- 		},
+	-- 		mapping = {
+	-- 			toggle_bg = nil,
+	-- 		},
+	-- 		groups = {},
+	-- 	})
+	--
+	-- 	vim.opt.background = "dark"
+	-- 	vim.cmd("colorscheme caret")
 	-- end,
 	priority = 1000, --load this plugin in a higher priority
 }
