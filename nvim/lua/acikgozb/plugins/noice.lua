a49d549487653172454ced0e67312d7e7517d54c
acikgozb <berkacikgoz@armut.com>
2024-02-10 21:51:24 +0300

    Add noice.nvim for cmdline

---
+++ nvim/lua/acikgozb/plugins/noice.lua
@@ -0,0 +1,28 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("noice").setup({
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+			},
+			messages = {
+				enabled = false,
+			},
+			popupmenu = {
+				enabled = false,
+			},
+		})
+	end,
+}
