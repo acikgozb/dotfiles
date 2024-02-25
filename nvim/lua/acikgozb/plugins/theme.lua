5b9c177da14fe404db6bb4b7d09805bcf333fc17
acikgozb <berkacikgoz@armut.com>
2024-02-25 16:52:38 +0300

    Change theme with Github instead of Rose-Pine
    Nvim and tmux themes are changed with Github.
    
    Only bg of tmux bar is changed to Github theme background to make them sync.
    Old Rose Pine nvim theme is commented out as a documentation for future uses.

--- nvim/lua/acikgozb/plugins/theme.lua
+++ nvim/lua/acikgozb/plugins/theme.lua
@@ -32,54 +32,54 @@ return {
 	--
 	-- vim.cmd.colorscheme("catppuccin")
 	-- end,
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({
-			variant = "main",
-			dark_variant = "main",
-			dim_inactive_windows = false,
-			extend_background_behind_borders = true,
-			enable = {
-				terminal = true,
-				legacy_highlights = true,
-				migrations = true,
-			},
-			styles = {
-				bold = true,
-				italic = true,
-				transparency = true,
-			},
-			groups = {
-				border = "muted",
-				link = "iris",
-				panel = "surface",
-				error = "love",
-				hint = "iris",
-				info = "foam",
-				note = "pine",
-				todo = "rose",
-				warn = "gold",
-				git_add = "foam",
-				git_change = "rose",
-				git_delete = "love",
-				git_dirty = "rose",
-				git_ignore = "muted",
-				git_merge = "iris",
-				git_rename = "pine",
-				git_stage = "iris",
-				git_text = "rose",
-				git_untracked = "subtle",
-				h1 = "iris",
-				h2 = "foam",
-				h3 = "rose",
-				h4 = "gold",
-				h5 = "pine",
-				h6 = "foam",
-			},
-		})
-		vim.cmd("colorscheme rose-pine")
-	end,
+	-- "rose-pine/neovim",
+	-- name = "rose-pine",
+	-- config = function()
+	-- 	require("rose-pine").setup({
+	-- 		variant = "main",
+	-- 		dark_variant = "main",
+	-- 		dim_inactive_windows = false,
+	-- 		extend_background_behind_borders = true,
+	-- 		enable = {
+	-- 			terminal = true,
+	-- 			legacy_highlights = true,
+	-- 			migrations = true,
+	-- 		},
+	-- 		styles = {
+	-- 			bold = true,
+	-- 			italic = true,
+	-- 			transparency = true,
+	-- 		},
+	-- 		groups = {
+	-- 			border = "muted",
+	-- 			link = "iris",
+	-- 			panel = "surface",
+	-- 			error = "love",
+	-- 			hint = "iris",
+	-- 			info = "foam",
+	-- 			note = "pine",
+	-- 			todo = "rose",
+	-- 			warn = "gold",
+	-- 			git_add = "foam",
+	-- 			git_change = "rose",
+	-- 			git_delete = "love",
+	-- 			git_dirty = "rose",
+	-- 			git_ignore = "muted",
+	-- 			git_merge = "iris",
+	-- 			git_rename = "pine",
+	-- 			git_stage = "iris",
+	-- 			git_text = "rose",
+	-- 			git_untracked = "subtle",
+	-- 			h1 = "iris",
+	-- 			h2 = "foam",
+	-- 			h3 = "rose",
+	-- 			h4 = "gold",
+	-- 			h5 = "pine",
+	-- 			h6 = "foam",
+	-- 		},
+	-- 	})
+	-- 	vim.cmd("colorscheme rose-pine")
+	-- end,
 	-- "loctvl842/monokai-pro.nvim",
 	-- config = function()
 	-- 	require("monokai-pro").setup({
@@ -150,5 +150,20 @@ return {
 	-- 	vim.opt.background = "dark"
 	-- 	vim.cmd("colorscheme caret")
 	-- end,
+	"projekt0n/github-nvim-theme",
+	lazy = false,
 	priority = 1000, --load this plugin in a higher priority
+	config = function()
+		require("github-theme").setup({
+			options = {
+				styles = {
+					comments = "italic",
+					keywords = "bold",
+					types = "italic, bold",
+				},
+			},
+		})
+
+		vim.cmd("colorscheme github_dark_tritanopia")
+	end,
 }
