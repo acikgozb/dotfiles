c90c4ce697e1a11b138be79c24cf7f0293a211fb
acikgozb <berkacikgoz@armut.com>
2024-03-01 10:24:01 +0300

    Replace null-ls with conform.nvim
    Instead of using null-ls, conform.nvim is used again.
    
    I found out that configuring conform to format TS files is much, much easier than null-ls.
    Also, it's easier to understand now that which plugin does what, since null-ls is much more than a formatter, it creates a confusion.

---
+++ nvim/lua/acikgozb/plugins/format.lua
@@ -0,0 +1,33 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { { "eslint_d", "prettierd", "prettier" } },
+				typescript = { { "eslint_d", "prettierd", "prettier" } },
+				json = { { "eslint_d", "prettierd", "prettier" } },
+				html = { { "eslint_d", "prettierd", "prettier" } },
+				markdown = { { "prettierd", "prettier" } },
+				go = { "golines", "gofumpt", "goimports" },
+				csharp = { "csharpier" },
+				sh = { "beautysh" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set("n", "<Leader>gf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, {})
+	end,
+}
