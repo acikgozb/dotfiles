15164630f698d6f076f2f44466611edbdbcac632
acikgozb <berkacikgoz@armut.com>
2024-03-03 01:13:31 +0300

    Remove .net specific dap configs
    .net debugging experience was sub optimal at best.
    Therefore, it has been removed for now.
    A more complete solution will be implemented later.

--- nvim/lua/acikgozb/plugins/debugging.lua
+++ nvim/lua/acikgozb/plugins/debugging.lua
@@ -1,102 +1,19 @@
-vim.g.dotnet_build_project = function()
-	local default_path = vim.fn.getcwd() .. "/"
-	if vim.g["dotnet_last_proj_path"] ~= nil then
-		default_path = vim.g["dotnet_last_proj_path"]
-	end
-
-	local path = vim.fn.input("Path to your *proj file", default_path, "file")
-	vim.g["dotnet_last_proj_path"] = path
-
-	local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
-
-	print("")
-	print("Cmd to execute: " .. cmd)
-
-	local f = os.execute(cmd)
-	if f == 0 then
-		print("The project has been built.")
-	else
-		print("The project has been failed to build.")
-	end
-end
-
-vim.g.dotnet_get_dll_path = function()
-	local request = function()
-		return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
-	end
-
-	if vim.g["dotnet_last_dll_path"] == nil then
-		vim.g["dotnet_last_dll_path"] = request()
-	else
-		if
-			vim.fn.confirm("Do you want to change the path to dll?\n" .. vim.g["dotnet_last_dll_path"], "&yes\n&no", 2)
-			== 1
-		then
-			vim.g["dotnet_last_dll_path"] = request()
-		end
-	end
-
-	return vim.g["dotnet_last_dll_path"]
-end
-
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
-		-- "rcarriga/nvim-dap-ui",
 		"leoluz/nvim-dap-go",
 		"theHamsta/nvim-dap-virtual-text",
 	},
 	config = function()
 		local dap = require("dap")
 		local dap_virtual_text = require("nvim-dap-virtual-text")
-
-		-- local dap_ui = require("dapui")
 		local dap_go = require("dap-go")
 
-		-- Create a debugging adapter for C#
-		local home_path = os.getenv("HOME")
-		dap.adapters.coreclr = {
-			type = "executable",
-			command = home_path .. "/.local/share/nvim/mason/bin/netcoredbg",
-			args = { "--interpreter=vscode" }, -- Unfortunately, we need to add VSCode here.
-		}
-
-		-- Configure DAP to use the custom adapter we initialized for C#
-		dap.configurations.cs = {
-			{
-				type = "coreclr",
-				name = "launch - netcoredbg",
-				request = "launch",
-				program = function()
-					if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then
-						vim.g.dotnet_build_project()
-					end
-					return vim.g.dotnet_get_dll_path()
-				end,
-			},
-		}
-
-		-- dap_ui.setup()
 		dap_virtual_text.setup({
 			virt_text_pos = "eol",
 		})
-		dap_go.setup()
 
-		-- dap.listeners.before.attach.dapui_config = function()
-		-- 	dap_ui.open()
-		-- end
-		--
-		-- dap.listeners.before.launch.dapui_config = function()
-		-- 	dap_ui.open()
-		-- end
-		--
-		-- dap.listeners.before.event_terminated.dapui_config = function()
-		-- 	dap_ui.close()
-		-- end
-		--
-		-- dap.listeners.before.event_exited.dapui_config = function()
-		-- 	dap_ui.close()
-		-- end
+		dap_go.setup()
 
 		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
 		vim.keymap.set("n", "<Leader>dta", dap.clear_breakpoints, {})
@@ -114,9 +31,5 @@ return {
 		end, {})
 		vim.keymap.set("n", "<Leader>duh", require("dap.ui.widgets").hover, {})
 		vim.keymap.set("n", "<Leader>dgt", dap_go.debug_test, {})
-
-		-- dap ui keymaps, might delete these if UI is not necessary
-		-- vim.keymap.set("n", "<Leader>duo", dap_ui.open, {})
-		-- vim.keymap.set("n", "<Leader>duc", dap_ui.close, {})
 	end,
 }
