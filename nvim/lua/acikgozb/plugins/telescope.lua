98da6b7b0e0645fc48be2e931e79e19056e19193
acikgozb <berkacikgoz@armut.com>
2024-04-15 08:47:51 +0300

    Update telescope.nvim to use the latest version
    Telescope tag is removed.

--- nvim/lua/acikgozb/plugins/telescope.lua
+++ nvim/lua/acikgozb/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
 		dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep", "nvim-telescope/telescope-fzf-native.nvim" },
 		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build bulid --config Release && cmake --install build --prefix build",
 		config = function()
