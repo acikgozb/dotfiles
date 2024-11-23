81d8c62870b41bd5941caf8014403c71f6a2812c
acikgozb <berkacikgoz@armut.com>
2024-11-23 20:29:25 +0300

    [update-nvim] Update conform.nvim configuration
    Updating conform.nvim brought in some breaking changes.
    Therefore, the conform configuration is updated to align with the
    changes.

--- nvim/lua/acikgozb/plugins/format.lua
+++ nvim/lua/acikgozb/plugins/format.lua
@@ -1,28 +1,32 @@
 return {
 	"stevearc/conform.nvim",
 	config = function()
-		local conform = require("conform")
-
-		conform.setup({
+		require('conform').setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { { "eslint_d", "prettier" } },
-				typescript = { { "eslint_d", "prettier" } },
+				javascript = {  "eslint_d" },
+				typescript = {  "eslint_d" },
 				json = { "fixjson", "jq" },
-				html = { { "eslint_d", "prettier" } },
-				markdown = { { "prettier" } },
+				html = {  "eslint_d" },
+				markdown = {  "prettier"  },
 				go = { "gofumpt", "goimports" },
 				csharp = { "csharpier" },
 				bash = { "beautysh" },
 				yaml = { "yamlfmt" },
-				["yaml.ansible"] = { "yamlfmt", "ansible-lint" },
 				sql = { "sqlfmt" },
 				tf = { "terraform_fmt" },
 			},
 			format_on_save = {
-				lsp_fallback = true,
+				lsp_format = "fallback",
 				timeout_ms = 500,
 			},
+      -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+      log_level = vim.log.levels.ERROR,
+      -- Conform will notify you when a formatter errors
+      notify_on_error = true,
+      -- Conform will notify you when no formatters are available for the buffer
+      notify_no_formatters = true,
+      -- Custom formatters and overrides for built-in formatters
 			formatters = {
 				yamlfmt = {
 					prepend_args = { "-conf", os.getenv("XDG_CONFIG_HOME") .. "/yamlfmt/.yamlfmt.yml" },
