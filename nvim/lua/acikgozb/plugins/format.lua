bc201946c8831975e3ca499bdeb41bcc67b4851b
acikgozb <berkacikgoz@armut.com>
2024-11-24 17:02:19 +0300

    [update-nvim] Finalize aesthetic changes
    - Font size is incresed via kitty.conf
    - Active tmux window background color is changed to be in sync with
    Neovim theme.

--- nvim/lua/acikgozb/plugins/format.lua
+++ nvim/lua/acikgozb/plugins/format.lua
@@ -1,14 +1,16 @@
 return {
 	"stevearc/conform.nvim",
 	config = function()
-		require('conform').setup({
+		local conform = require("conform")
+
+		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = {  "eslint_d" },
-				typescript = {  "eslint_d" },
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
 				json = { "fixjson", "jq" },
-				html = {  "eslint_d" },
-				markdown = {  "prettier"  },
+				html = { "eslint_d" },
+				markdown = { "prettier" },
 				go = { "gofumpt", "goimports" },
 				csharp = { "csharpier" },
 				bash = { "beautysh" },
@@ -20,13 +22,13 @@ return {
 				lsp_format = "fallback",
 				timeout_ms = 500,
 			},
-      -- Set the log level. Use `:ConformInfo` to see the location of the log file.
-      log_level = vim.log.levels.ERROR,
-      -- Conform will notify you when a formatter errors
-      notify_on_error = true,
-      -- Conform will notify you when no formatters are available for the buffer
-      notify_no_formatters = true,
-      -- Custom formatters and overrides for built-in formatters
+			-- Set the log level. Use `:ConformInfo` to see the location of the log file.
+			log_level = vim.log.levels.ERROR,
+			-- Conform will notify you when a formatter errors
+			notify_on_error = true,
+			-- Conform will notify you when no formatters are available for the buffer
+			notify_no_formatters = true,
+			-- Custom formatters and overrides for built-in formatters
 			formatters = {
 				yamlfmt = {
 					prepend_args = { "-conf", os.getenv("XDG_CONFIG_HOME") .. "/yamlfmt/.yamlfmt.yml" },
