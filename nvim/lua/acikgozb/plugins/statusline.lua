7573b89077f28ac468d949d237bef8e1dec49e06
acikgozb <berkacikgoz@armut.com>
2024-11-24 17:01:02 +0300

    [update-nvim] Add custom lualine.nvim theme
    A custom theme is written to disable the ugly gray coloring for section
    C of lualine.nvim.

--- nvim/lua/acikgozb/plugins/statusline.lua
+++ nvim/lua/acikgozb/plugins/statusline.lua
@@ -4,10 +4,51 @@ return {
 	config = function()
 		local lualine = require("lualine")
 
+		local colors = {
+			blue = "#66b2b2",
+			black = "#1c1c1c",
+			green = "#1bfd9c",
+			gray = "#404040",
+			white = "#d1d1d1",
+		}
+
+		local theme = {
+			normal = {
+				a = { bg = colors.blue, fg = colors.black, gui = "bold" },
+				b = { bg = colors.black, fg = colors.blue },
+				c = { bg = colors.black },
+			},
+			visual = {
+				a = { bg = colors.black, fg = colors.green, gui = "bold" },
+				b = { bg = colors.black, fg = colors.white },
+				c = { bg = colors.black },
+			},
+			insert = {
+				a = { bg = colors.black, fg = colors.green, gui = "bold" },
+				b = { bg = colors.black, fg = colors.white },
+				c = { bg = colors.black },
+			},
+			command = {
+				a = { bg = colors.black, fg = colors.green, gui = "bold" },
+				b = { bg = colors.black, fg = colors.white },
+				c = { bg = colors.black },
+			},
+			inactive = {
+				a = { bg = colors.black, fg = colors.green, gui = "bold" },
+				b = { bg = colors.black, fg = colors.white },
+				c = { bg = colors.black },
+			},
+			replace = {
+				a = { bg = colors.black, fg = colors.green, gui = "bold" },
+				b = { bg = colors.black, fg = colors.white },
+				c = { bg = colors.black },
+			},
+		}
+
 		lualine.setup({
 			options = {
 				icons_enabled = true,
-				theme = "auto",
+				theme = theme,
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 			},
